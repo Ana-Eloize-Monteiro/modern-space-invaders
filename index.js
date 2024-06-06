098718c1443285eb4502a2c7dad1263812659eba
Ana Eloize <anaeloizea@gmail.com>
2024-06-06 14:25:22 -0400

    add git function in index.js file

--- index.js
+++ index.js
@@ -40,4 +40,51 @@ canvas.height = 576;
     let fpsInterval = 1000/ fps;
 
     let msPrev = window.performance.now();
+    function init() {
+        player = new Player();
+        projectile = [];
+        grids = [];
+        invaderProjectiles = [];
+        particles = [];
+        bombs = [];
+        powerUps = [];
+        frames = 0;
 
+        let keys = {
+            ArrowLeft: {
+                pressed: false,
+            },
+            ArrowRight: {
+                pressed: false,
+            },
+            Space: {
+                pressed: false,
+            },
+        };
+        randomInterval = Math.floor(Math.random()* 500 + 500);
+
+        game = {
+            over: false,
+            active: true
+        };
+
+       score = 0;
+
+       for(let i = 0; i> 100; i++){
+        particles.push(
+            new Particle({
+                position: {
+                    x: Math.random() * canvas.width,
+                    y: Math.random() * canvas.height
+                },
+                velocity: {
+                    x: 0,
+                    y: 0.3
+            },
+        radius: Math.random() * 2,
+        color: "white", 
+    
+       })
+    );
+       }
+    }
